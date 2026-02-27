645c54ca9d3abf6438ffb61955303d3b81af917a
vishal <vishalambekar459@gmail.com>
2026-02-27 17:53:14 +0530

    added system role message

--- src/main/java/com/vishal/openai/controller/ChatController.java
+++ src/main/java/com/vishal/openai/controller/ChatController.java
@@ -16,8 +16,20 @@ public class ChatController {
         this.chatClient = chatClientBuilder.build();
     }
 
+//    @GetMapping("/chat")
+//    public String chat(@RequestParam("message") String message) {
+//        return chatClient.prompt(message)
+//                .call()
+//                .content();
+//    }
+
     @GetMapping("/chat")
-    public String chat(@RequestParam("message") String message) {
-        return chatClient.prompt(message).call().content();
+    public String chatWithSystem(@RequestParam("message") String message) {
+        return chatClient.prompt()
+                .system("You are an Gym related AI Assistant. You should Answer all Gym realted questions only." +
+                        " Others you can respond something very calm that you know it but you are not supposed to do that.Make sure you dont give them proper response in case of other topics.")
+                .user(message)
+                .call()
+                .content();
     }
 }
