b9a43997ea5f09ff8ccb91066609cf6689238b99
vishal <vishalambekar459@gmail.com>
2026-04-12 22:54:11 +0530

    Chat options added

--- src/main/java/com/vishal/openai/config/ChatClientConfig.java
+++ src/main/java/com/vishal/openai/config/ChatClientConfig.java
@@ -2,6 +2,8 @@ package com.vishal.openai.config;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.ai.ollama.api.OllamaChatOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,9 +12,14 @@ public class ChatClientConfig {
 
     @Bean
     public ChatClient chatClient(ChatClient.Builder chatClientBuilder){
+        OllamaChatOptions chatOptions = OllamaChatOptions.builder()
+                .temperature(0.1)
+                .build();
+
         return chatClientBuilder
                 .defaultAdvisors(new SimpleLoggerAdvisor())
                 .defaultAdvisors(new TokenUsageAuditAdvicer())
+                .defaultOptions(chatOptions)
                 .defaultSystem("You are an Gym related AI Assistant. You should Answer all Gym realted questions only.")
 //                .defaultUser("This is default user message")
                 .build();
