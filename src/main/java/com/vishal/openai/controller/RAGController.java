1bdb7d6e35c797265d4eb77cf76617003574e4be
vishal <vishalambekar459@gmail.com>
2026-07-02 23:49:35 +0530

    rag controller and RandomDataLoader added

---
+++ src/main/java/com/vishal/openai/controller/RAGController.java
@@ -0,0 +1,88 @@
+package com.vishal.openai.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
+
+@RestController
+@RequestMapping("/api/rag")
+public class RAGController {
+
+    private final ChatClient chatClient;
+    private final VectorStore vectorStore;
+
+    @Value("classpath:/promptTemplate/systemPromptRandomDataTemplate.st")
+    private Resource promptTemplate;
+
+    @Value("classpath:/promptTemplate/systemPromptTemplate.st")
+    private Resource hrSystemTemplate;
+
+    public RAGController(@Qualifier("chatMemoryClient") ChatClient chatClient,
+            VectorStore vectorStore) {
+        this.chatClient = chatClient;
+        this.vectorStore = vectorStore;
+    }
+
+    @GetMapping("/random/chat")
+    public ResponseEntity<String> randomChat(@RequestHeader("username") String username,
+            @RequestParam("message") String message) {
+        SearchRequest searchRequest = SearchRequest.builder().query(message).topK(3).similarityThreshold(0.5).build();
+
+        List<Document> similarDocs =  vectorStore.similaritySearch(searchRequest);
+
+        String similarContext = similarDocs.stream()
+                .map(Document::getText)
+                .collect(Collectors.joining(System.lineSeparator()));
+
+        String answer = chatClient.prompt()
+                .system(promptSystemSpec -> promptSystemSpec.text(promptTemplate)
+                        .param("documents", similarContext))
+                .advisors(a -> a.param(CONVERSATION_ID, username))
+                .user(message)
+                .call().content();
+        return ResponseEntity.ok(answer);
+    }
+
+    @GetMapping("/document/chat")
+    public ResponseEntity<String> documentChat(@RequestHeader("username") String username,
+            @RequestParam("message") String message) {
+       /* SearchRequest searchRequest =
+                SearchRequest.builder().query(message).topK(3).similarityThreshold(0.5).build();
+        List<Document> similarDocs =  vectorStore.similaritySearch(searchRequest);
+        String similarContext = similarDocs.stream()
+                .map(Document::getText)
+                .collect(Collectors.joining(System.lineSeparator()));*/
+        String answer = chatClient.prompt()
+                /*.system(promptSystemSpec -> promptSystemSpec.text(hrSystemTemplate)
+                                .param("documents", similarContext))*/
+                .advisors(a -> a.param(CONVERSATION_ID, username))
+                .user(message)
+                .call().content();
+        return ResponseEntity.ok(answer);
+    }
+
+//    @GetMapping("/web-search/chat")
+//    public ResponseEntity<String> webSearchChat(@RequestHeader("username")
+//    String username, @RequestParam("message") String message) {
+//        String answer = webSearchchatClient.prompt()
+//                .advisors(a -> a.param(CONVERSATION_ID, username))
+//                .user(message)
+//                .call().content();
+//        return ResponseEntity.ok(answer);
+//    }
+}
